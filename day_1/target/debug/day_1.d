cf58849ac3db86f6545c56856d2180fa70019565
nebyu08 <nebiyuyouhannes08@gmail.com>
2025-12-01 09:56:25 +0300

    fix:add target ignore

--- day_1/target/debug/day_1.d
+++
@@ -1,1 +0,0 @@
-/home/nebiyu/Documents/github_projects/AOC_2025/day_1/target/debug/day_1: /home/nebiyu/Documents/github_projects/AOC_2025/day_1/src/main.rs
--- day_1/target/debug/deps/day_1-2f1e0f5eee73cea0.d
+++
@@ -1,5 +0,0 @@
-/home/nebiyu/Documents/github_projects/AOC_2025/day_1/target/debug/deps/day_1-2f1e0f5eee73cea0.d: src/main.rs
-
-/home/nebiyu/Documents/github_projects/AOC_2025/day_1/target/debug/deps/day_1-2f1e0f5eee73cea0: src/main.rs
-
-src/main.rs:
