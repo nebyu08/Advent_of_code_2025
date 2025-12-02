83a38d4fb6fee3d3ac71d890d99249bd95b458a6
nebyu08 <nebiyuyouhannes08@gmail.com>
2025-12-02 15:32:44 +0300

    add:day 1 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,7 +1,6 @@
 mod p1;
 
 fn main() {
-    p1::p1();
-    // let contents= p1::p1();
-    // println!("File contents: {}", contents);
+    p1::p1(); //part one
+    p1::p2(); //part two
 }
--- day_2/src/p1.rs
+++ day_2/src/p1.rs
@@ -5,7 +5,7 @@ pub fn p1() {
     let file = File::open("data/input.txt").expect("Unable to open file");
     let reader = io::BufReader::new(file);
     let mut repition: Vec<i64> = vec![];
-    let mut total_sum:i64= 0;
+    let mut total_sum: i64 = 0;
 
     for line in reader.lines() {
         let line = line.expect("Unable to read line");
@@ -39,7 +39,58 @@ pub fn p1() {
     }
 }
 
-pub fn split_into_halves(s: &str) -> (&str, &str) {
+fn split_into_halves(s: &str) -> (&str, &str) {
     let mid = s.len() / 2;
     s.split_at(mid)
+}
+
+pub fn p2() {
+    let file = File::open("data/input.txt").expect("Unable to open file");
+    let reader = io::BufReader::new(file);
+    let mut repition: Vec<i64> = vec![];
+    let mut total_sum: i64 = 0;
+
+    for line in reader.lines() {
+        let line = line.expect("Unable to read line");
+
+        for pair in line.split(',') {
+            let bounds: Vec<&str> = pair.split('-').collect();
+            let start: i64 = bounds[0].parse().expect("Unable to parse start");
+            let end: i64 = bounds[1].parse().expect("Unable to parse end");
+
+            for num in start..=end {
+                let str_num = num.to_string();
+                if is_repeating(&str_num) {
+                    // println!("Found repitition: {}", num);
+                    repition.push(num);
+                }
+            }
+        }
+
+        for i in &repition {
+            total_sum += i;
+        }
+
+        println!("Total sum of repititions: {}", total_sum);
+
+        // println!("{:?}", pairs);
+    }
+}
+
+fn is_repeating(s: &str) -> bool {
+    let len = s.len();
+        for size in 1..=(len / 2) {
+            if len % size != 0 {
+                continue;
+            }
+            let part = s[0..size].to_string();
+            let repeats = len / size;
+            let constructed = part.repeat(repeats);
+
+            if constructed == s {
+                return true;
+            }
+        }
+
+    false
 }
