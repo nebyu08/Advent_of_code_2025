ae47975b907b81456bd6ad55b8c4eb3ed2e1448c
nebyu08 <nebiyuyouhannes08@gmail.com>
2025-12-02 14:21:08 +0300

    day 2 part 1

---
+++ day_2/src/main.rs
@@ -0,0 +1,7 @@
+mod p1;
+
+fn main() {
+    p1::p1();
+    // let contents= p1::p1();
+    // println!("File contents: {}", contents);
+}
---
+++ day_2/src/p1.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+
+pub fn p1() {
+    let file = File::open("data/input.txt").expect("Unable to open file");
+    let reader = io::BufReader::new(file);
+    let mut repition: Vec<i64> = vec![];
+    let mut total_sum:i64= 0;
+
+    for line in reader.lines() {
+        let line = line.expect("Unable to read line");
+
+        for pair in line.split(',') {
+            let bounds: Vec<&str> = pair.split('-').collect();
+            let start: i64 = bounds[0].parse().expect("Unable to parse start");
+            let end: i64 = bounds[1].parse().expect("Unable to parse end");
+
+            for num in start..=end {
+                let str_num = num.to_string();
+                if str_num.len() % 2 != 0 {
+                    continue;
+                }
+
+                let (left_num, right_num) = split_into_halves(str_num.as_str());
+                if left_num == right_num {
+                    println!("Found repitition: {}", num);
+                    repition.push(num);
+                }
+            }
+        }
+
+        for i in &repition {
+            total_sum += i;
+        }
+
+        println!("Total sum of repititions: {}", total_sum);
+
+        // println!("{:?}", pairs);
+    }
+}
+
+pub fn split_into_halves(s: &str) -> (&str, &str) {
+    let mid = s.len() / 2;
+    s.split_at(mid)
+}
