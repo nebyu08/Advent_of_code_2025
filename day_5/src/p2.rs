1a366163c2238aa546719b8fb031a0f8aec88903
nebyu08 <nebiyuyouhannes08@gmail.com>
2025-12-05 21:56:51 +0300

    check point

--- day_5/src/p2.rs
+++ day_5/src/p2.rs
@@ -4,61 +4,48 @@ pub fn p2() {
     let raw_lines: Vec<&str> = include_str!("../data/input.txt").lines().collect();
 
     let mut numbers_to_check: Vec<u128> = Vec::new();
-    // let mut count_exists:u128=0;
-    let mut data: Vec<(u128, u128)> = Vec::new();
-    let mut total_numers: Vec<u128> = Vec::new();
+    let mut ranges_only: Vec<(u128, u128)> = Vec::new();
 
-    let ranges_only: Vec<(u128, u128)> = raw_lines
-        .iter()
-        .filter_map(|line| {
-            if !line.contains('-') {
-                return None;
-            }
-
-            let nums = line
-                .split('-')
-                .map(|x| x.parse::<u128>().unwrap())
-                .collect::<Vec<u128>>();
-
-            (nums.len() == 2).then(|| (nums[0], nums[1]))
-        })
-        .collect();
-
-    // println!("the ranges only are {:?}",ranges_only);
-
+    // Parse input
     for line in &raw_lines {
         if line.contains('-') {
-            continue;
-        }
-        if let Ok(num) = line.trim().parse::<u128>() {
+            let nums: Vec<u128> = line
+                .split('-')
+                .filter_map(|x| x.parse::<u128>().ok())
+                .collect();
+            if nums.len() == 2 {
+                ranges_only.push((nums[0], nums[1]));
+            }
+        } else if let Ok(num) = line.trim().parse::<u128>() {
             numbers_to_check.push(num);
         }
     }
 
-    // println!("the individual value are {:?}",numbers_to_check);
+    // Use HashSet to track which ranges contain each number
+    // let mut acceptable_ranges = HashSet::new();
+    let mut good_rangess:Vec<(u128,u128)>=Vec::new();
 
-    for i in numbers_to_check {
-        for &j in &ranges_only {
-            let (temp_lower, temp_upper) = j;
-            let range_inclusive = temp_lower..=temp_upper;
-            if range_inclusive.contains(&i) {
-                data.push(j);
-                // count_exists+=1;
+    for num in numbers_to_check {
+        for &(lower, upper) in &ranges_only {
+            if num >= lower && num <= upper {
+                // Instead of storing the entire range, just add the numbers we need
+                // acceptable_ranges.insert(num);
+                good_rangess.push((lower,upper));
                 break;
             }
         }
     }
 
-    for (lower, upper) in data {
-        for i in lower..=upper {
-            total_numers.push(i);
+    //count
+    let mut temp_numbers:HashSet<u128>=HashSet::new();
+
+    for (lower,upper) in good_rangess{
+        for i in lower..=upper{
+            temp_numbers.insert(i);
         }
-        // let temp_count: u128=temp.count().try_into().unwrap();
-        // count_exists+=temp_count;
-    }
+    } 
 
-    // let counting=total_numers
-    let number_set:HashSet<u128>=total_numers.into_iter().collect();
+  
 
-    println!("the total count is {}",number_set.len());
+    println!("the total count is {}", temp_numbers.len());
 }
